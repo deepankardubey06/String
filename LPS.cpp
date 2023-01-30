653ead7bcdfcc7251395a268827774243e75a064
Deepankar <deepankardubey06@gmail.com>
2023-01-30 11:45:58 +0530

    LPS modified

--- LPS.cpp
+++ LPS.cpp
@@ -3,41 +3,26 @@
 #include <string>
 using namespace std;
 
-string getLps(string s){
+int getLps(string s){
 	int i = 1;
 	int index = 0;
-	vector<int> lps(s.length());
+	int n = s.length();
 
 	while(i < s.length()){
 		if(s[i] == s[index]){
-			lps[i] = index + 1;
-			i++;
-			index++;
+			++i;
+			++index;
 		}
 		else{
-			if(index != 0){
-				index = lps[index - 1];
-			}
-			else{
-				lps[i] = 0;
-				i++;
-			}
+			i = i - index + 1;
+			index = 0;
 		}
 	}
-	int maxi = 0;
-	for(int i=0; i<lps.size(); i++){
-		maxi = max(maxi, lps[i]);
-	}
-	string res = s.substr(0,maxi);
-	return res;
+	return index>n/2? index/2:index;
 }
 
 int main(){
-	cout<<getLps("ababab");
-
-	for(int i=0; i<arr.size(); i++){
-		cout<<arr[i]<<" ";
-	}
-
+	cout<<getLps("ababab")<<endl;
+	cout<<getLps("level");
 	return 0;
 }
